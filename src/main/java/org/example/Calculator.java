dbb099af2ac669b9300106732ce611a5c6f2cbf3
Yaroslav3390 <yaroslavkutnenko@gmail.com>
2023-06-25 13:54:36 +0300

    homework07

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,10 +3,22 @@ package org.example;
 public class Calculator {
 
     public int add(int num1, int num2){
-        return num1 + num2;
+        int result = num1 + num2;
+        return result;
 
         }
-    public int sub(int num1, int num2){
+    public int subtraction(int num1, int num2){
+
         return num1 - num2;
+
+    }
+
+    public double multiplication(double num1, double num2) {
+
+        return num1 * num2;
+    }
+
+    public double division(double num1, double num2){
+        return num1 % num2;
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,6 +1,34 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
 public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
